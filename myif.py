da07bbeb87578c2f63b2d6c74bd965f1de70ec97
BeakHo -hub <jiwon020907@gmail.com>
2025-04-11 02:58:26 +0000

    25.04.11

--- myif.py
+++ myif.py
@@ -4,20 +4,17 @@
 # 만약에 2를 입력하면 2번 캐릭터 출력
 # 3을 입력하면 3번 캐릭터 푸렭
 # 잘못 입력하면 잘못 입력했다고 출력
-print("그림 출력 프로그램")
-print("================")
-print("1")
-print("2")
-print("3")
-print("4")
-print("0을 입력하면 종료")
-print("================")
-while True:
-    A = input("원하는 그림 번호 입력 (0 입력 시 종료): ")
-    if A == '0':
-        print("프로그램을 종료합니다.")
-        break
-    if A == '1':
+def print_menu():
+    print("그림 출력 프로그램")
+    print("================")
+    print("1")
+    print("2")
+    print("3")
+    print("4")
+    print("0을 입력하면 종료")
+    print("================")
+def print_picture(number):
+    if number == '1':
         midoriya = r"""
     ⣏⡹⣬⣍⣣⣭⣙⣬⣍⢧⣻⣟⣿⡿⣿⢿⣿⢿⡿⣿⢿⡿⣿⢿⡿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
     ⢢⠑⡦⣌⠱⣌⡙⡚⡔⢪⢿⣾⣽⡿⣽⣻⣿⣿⣿⣽⣯⣿⡽⣯⢿⣽⣻⣽⣻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
@@ -47,7 +44,7 @@ while True:
         for i in range(5):
             print(midoriya)
 
-    elif A =='2':
+    elif number =='2':
         midoriya2 = r"""
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⠈⠹⣶⣤⣬⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣷⣤⣠⣤⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⡻⠛⠳⠆⢠⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -72,7 +69,7 @@ while True:
         for i in range(5):
             print(midoriya2)
 
-    elif A == '3':
+    elif number == '3':
         midoriya3 = r"""
     ⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
     ⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⡿⣽⢯⣟⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣽⡿⣽⣿⣿⣿⣿⣿⣿⣿⣿
@@ -97,7 +94,7 @@ while True:
         for i in range(5):
             print(midoriya3)
 
-    elif A == '4':
+    elif number == '4':
         midoriya4 = r"""
 
     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
@@ -124,5 +121,17 @@ while True:
         for i in range(5):
             print(midoriya4)
     else:
-        print("잘못된 입력입니다. 1~4 또는 0을 입력하세요.")
+        print("해당 번호에 해당하는 그림이 없습니다 1~4 또는 0을 입력해주세요.")
 
+def main():
+    print_menu()
+    while True:
+        A = input("원하는 그림 번호 입력 (0 입력 시 종료): ")
+        if A == '0':
+            print("프로그램을 종료합니다.")
+            break
+        print_picture(A)
+
+
+# 프로그램 실행
+main()
